192daaa35f8e29ea5f499e01cb864ec97c1aa627
Clément ASBERT <clement.asbert@gmail.com>
2020-09-11 17:12:13 +0200

    ajout fichier sur les tableaux

---
+++ Tableaux-SWIFT.playground/Contents.swift
@@ -0,0 +1,30 @@
+var players = ["Clément", "Lucas", "Jean"]
+
+var roles: [String]
+roles = []
+
+// Ajout d'en un tableau avec la méthodes append
+
+roles.append("Main player")
+roles.append("Seconde player")
+roles.append("Seconde player")
+roles.append("Seconde player")
+roles.append("Seconde player")
+roles.append("Seconde player")
+roles.append("Seconde player")
+roles.append("Seconde player")
+roles.append("Seconde player")
+print(roles)
+
+//supression d'un élément dans un tableau avec la méthodes remove
+
+roles.remove(at: 0)
+print(roles)
+
+// Il y a énormement de méthodes remove pour pouvoir supprimé tous le tableaux ou que le premié ect...
+
+roles.removeFirst()
+roles.removeLast()
+roles.removeAll()
+print(roles)
+
