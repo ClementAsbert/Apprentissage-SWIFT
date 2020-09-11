f6c93916c7f480c1de30df18317582fa97adbe3d
Clément ASBERT <clement.asbert@gmail.com>
2020-09-11 17:08:41 +0200

    ajout du fichier sur les variable

---
+++ Variable-SWIFT.playground/Contents.swift
@@ -0,0 +1,18 @@
+// var pour declarer une variable
+var name = "Clément ASBERT"
+
+ print("Bonjour \(name)")
+
+
+//let pour déclaré une constante
+let birthCity = "Montbéliard"
+
+let departement : Int8 = 59
+
+let pi : Double
+pi = 3.1415926
+
+let n = 3
+
+//Converstion de type ici on convertie n en double
+print(Double(n) * pi)
